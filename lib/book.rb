f4d14dd05cdc4880997765db4366a00ad641a4b6
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-10-31 02:36:06 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+require 'pry'
 
+class Book
+  attr_accessor :title, :author, :author, :page_count, :genre
+  def initialize(title = "", author = " ", page_count = 0, genre = "")
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
+
+   def turn_page
+    puts "Flipping the page...wow, you read fast!"
+   end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  def initialize(brand = "", color = "", size = 0, material = "", condition = "")
+    @brand = brand
+    @color = color
+    @material = material
+    @condition = condition
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
